fe287111d46ca7d2d35baa6da75115d4d89f4e99
lauroteles <laurotfl@gmail.com>
2024-04-11 10:10:18 -0300

    Atualizando

--- co_admin.py
+++ co_admin.py
@@ -17,7 +17,6 @@ class Carteiras_co_admin():
         arquivo_final_completo = pd.merge(arquivo_final,controle_co_admin_df,on
                                             ='Conta',how='right').iloc[:-5,[0,2,4,6,10,16,11,20,21,22,-1]].rename(columns={'Valor':'PL'})
         coluna_final = arquivo_final_completo.columns[-1]
-
         arquivo_final_completo = arquivo_final_completo.rename(columns={coluna_final:'PL Planilha Controle'}).iloc[:,[0,2,3,5,6,7,8,1,4,9,10]]
         arquivo_final_completo = arquivo_final_completo[(arquivo_final_completo['Saldo']>1000)|(arquivo_final_completo['Saldo']<0)].sort_values(by='Saldo',ascending=False)
         return arquivo_final_completo
