49b3c746d3a5c6dbc972a3cc517884d4a17f4bdf
lauroteles <laurotfl@gmail.com>
2024-04-18 10:38:39 -0300

    atualizando posicao

--- assessores.py
+++ assessores.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import streamlit as st
 import datetime
+import numpy as np
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 from reportlab.lib.colors import Color
 from reportlab.platypus import SimpleDocTemplate, Image, Table, PageBreak, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
-
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from textwrap import wrap
 
 st.set_page_config(layout='wide')
 
@@ -16,8 +19,10 @@ ordens = pd.read_csv(r'C:\Users\lauro.telles\Desktop\Mesa_app_3\app_mesa_de_oper
 acompanhamentos_de_assessores = pd.read_csv(r'C:\Users\lauro.telles\Desktop\Mesa_app_3\app_mesa_de_opera-es_corrigido\acompanhamento_de_operacoes.csv').rename(
     columns={'Solicitada':'Data/Hora'})
 controle = pd.read_excel(r'C:\Users\lauro.telles\Desktop\Mesa_app_3\app_mesa_de_opera-es_corrigido\Controle de Contratos.xlsx',2,skiprows=1)
-
-ordens = ordens.iloc[:,:6]
+ordens['Valor'] = round(ordens['Qt. Executada']*ordens['Preço Médio'],2).astype(str).apply(lambda x: f'R$ {x}')
+print(ordens.info())
+ordens = ordens.iloc[:,[1,2,3,4,5,27]]
+st.dataframe(ordens)
 controle = controle.iloc[:-5,[1,2,4,5]]
 
 renda_variavel = pd.merge(ordens,controle,on='Conta',how='outer')
@@ -26,58 +31,41 @@ assesores_e_controle= assesores_e_controle[assesores_e_controle['Produto'].notnu
 renda_variavel = renda_variavel[renda_variavel['Ativo'].notnull()].rename(columns={'Ativo':'Descricao','Status':'Situacao','Direção':'Operacao'})
 arquivo_final = pd.concat([assesores_e_controle,renda_variavel]).rename(columns={'Data/Hora':'Data'}).drop(columns=['Requisitante','Produto'])
 arquivo_final['Data'] = dia_e_hora.strftime('%d/%m/%Y')
+arquivo_final = arquivo_final.drop(columns=['Cliente_x','Conta']).rename(columns={'Cliente_y':'Cliente'})
+def truncar_descricao(tabela,coluna,n_de_palvras):
 
+    if 'Descricao' in tabela.columns:
+        for indice, linha in tabela.iterrows():
+            tabela.at[indice, coluna] = linha[coluna][:n_de_palvras]
+    return tabela
 
-df = arquivo_final[arquivo_final['UF']=='DF']
-go = arquivo_final[arquivo_final['UF']=='GO']
-sul = arquivo_final[arquivo_final['UF']=='SUL']
+arquivo_final_truncado = truncar_descricao(arquivo_final,'Descricao',100)
+arquivo_final_truncado = truncar_descricao(arquivo_final,'Cliente',24)
+
+df = arquivo_final_truncado[arquivo_final_truncado['UF']=='DF']
+go = arquivo_final_truncado[arquivo_final_truncado['UF']=='GO']
+sul = arquivo_final_truncado[arquivo_final_truncado['UF']=='SUL']
 
 
 seletor_assessor_df = st.sidebar.selectbox('Selecione o Assessor DF',options=df['Assessor'].unique(),key='Assessor df')
 seletor_assessor_go = st.sidebar.selectbox('Selecione o Assessor GO',options=go['Assessor'].unique(),key='Assessor go')
 seletor_assessor_sul = st.sidebar.selectbox('Selecione o Assessor Sul',options=sul['Assessor'].unique(),key='Assessor sul')
 
+assessores_sul = list(sul['Assessor'].unique())
 
-df = df[df['Assessor']==seletor_assessor_df]
-sul = sul[sul['Assessor']==seletor_assessor_sul]
-go = go[go['Assessor']==seletor_assessor_go]
+df = df[df['Assessor']==seletor_assessor_df].reset_index(drop='index')
+sul = sul[sul['Assessor']==seletor_assessor_sul].reset_index(drop='index')
+go = go[go['Assessor']==seletor_assessor_go].reset_index(drop='index')
+sul = sul.drop(columns='UF')
+go = go.drop(columns='UF')
+df = df.drop(columns='UF')
 
 
-# def gerando_pdf(asssssor,data_dia,tabela):
-        
-#         c = canvas.Canvas(filename='relatorio.pdf',pagesize=letter)
-
-#         img1 = ImageReader("LOGO_BLUEMETRIX_VERTICAL jpg.jpg")
-#         c.drawImage(img1, 150, 600, width=250, height=200,mask='auto')
-
-#         # Configurações de fonte
-#         c.setFont('Times-Roman', 15)  # Fonte em negrito, tamanho 16
-#         c.setFillColor('black')
-#         c.drawString(230, 450, "Assessor")
-
-#         c.setFont('Helvetica-Bold', 20)  # Fonte em negrito, tamanho 16
-#         c.setFillColor('black')
-#         c.drawString(190, 600, f'{asssssor}')
-
-#         c.setFont('Times-Roman', 5)  # Fonte em negrito, tamanho 16
-#         c.setFillColor('black')
-#         c.drawString(190, 550, f'{data_dia}')
-
-#         data = [tabela.columns.values.tolist()] + tabela.values.tolist()
-#         t = Table(data)
-#         t.wrapOn(c, 50, 0)
-#         t.drawOn(c, 30, 150)
-    
-
-#         c.save()
-        
-
-#         return c
 def gerando_pdf(asssssor, data_dia, tabela):
-    filename = 'relatorio.pdf'
+    filename = f'relatorio__{assessor}__.pdf'
 
     # Cria um objeto SimpleDocTemplate para gerar o PDF
-    doc = SimpleDocTemplate(filename, pagesize=letter)
+    doc = SimpleDocTemplate(filename, pagesize=letter,topMargin=25)
     story = []
 
     # Adiciona a imagem ao início do PDF
@@ -92,13 +80,18 @@ def gerando_pdf(asssssor, data_dia, tabela):
     story.append(Paragraph(assessor_text, styles["Normal"]))
     story.append(Paragraph(data_text, styles["Normal"]))
 
-    # Adiciona a tabela ao PDF
-    data = [tabela.columns.values.tolist()] + tabela.values.tolist()
-    t = Table(data)
+    # # Define o estilo da tabela
+    table_style = [('GRID', (0, 0), (-1, -1), 1, 'grey'),
+               ('FONTNAME', (0, 0), (-1, -1), 'Helvetica'),
+               ('FONTSIZE', (0, 0), (-1, -1), 4.5),
+               ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+               ('WORDWRAP', (0, 0), (-1, -1)),
+               ('COLWIDTHS', (0, 0), (-1, -1), [1.2*inch, 1.2*inch, 2.4*inch, 1.2*inch, 0.5*inch, 1.2*inch, 1.2*inch]),
+    ]
 
-    # Define o estilo da tabela
-    style = [('GRID', (0, 0), (-1, -1), 1, 'BLACK')]
-    t.setStyle(style)
+    #t.setStyle(style)
+    data = [tabela.columns.values.tolist()] + tabela.values.tolist()
+    t = Table(data, style=table_style) 
 
     # Adiciona a tabela à história do PDF
     story.append(t)
@@ -121,4 +114,6 @@ st.dataframe(go)
 st.dataframe(sul)
 
 if st.button('Gerar Relatorio '):
-        gerar_pdf = gerando_pdf(sul['Assessor'].iloc[-1],dia_e_hora,sul)
+    for assessor in assessores_sul:
+            tabela_assessor = arquivo_final_truncado[arquivo_final_truncado['Assessor']==assessor]
+            gerar_pdf = gerando_pdf(assessor,dia_e_hora,tabela_assessor)
